27f281e68b560c89292ff687734416050a1626db
David Skinner <david.skinnera@gmail.com>
2014-03-22 16:57:09 -0400

    remove built folder from repo

--- project/target/classes/db/populateDB.sql
+++
@@ -1,6 +0,0 @@
---INSERT IGNORE INTO vets VALUES (1, 'James', 'Carter');
---INSERT IGNORE INTO vets VALUES (2, 'Helen', 'Leary');
---INSERT IGNORE INTO vets VALUES (3, 'Linda', 'Douglas');
---INSERT IGNORE INTO vets VALUES (4, 'Rafael', 'Ortega');
---INSERT IGNORE INTO vets VALUES (5, 'Henry', 'Stevens');
---INSERT IGNORE INTO vets VALUES (6, 'Sharon', 'Jenkins');
