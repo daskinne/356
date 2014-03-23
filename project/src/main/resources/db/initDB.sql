83360a29e1b2b21aa6d95933d1de9f08e766a95d
David Skinner <david.skinnera@gmail.com>
2014-03-23 15:12:36 -0400

    change schema

--- project/src/main/resources/db/initDB.sql
+++ project/src/main/resources/db/initDB.sql
@@ -9,9 +9,8 @@ USE `ece356` ;
 -- Table `ece356`.`user`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `ece356`.`user` (
-    `user_id` INT NOT NULL AUTO_INCREMENT,
+    `user_id` VARCHAR(45) NOT NULL,
     `password` VARCHAR(45) NOT NULL,
-    `username` VARCHAR(45) NOT NULL,
 	`first_name` VARCHAR(45) NOT NULL,
     PRIMARY KEY (`user_id`)
 )  ENGINE=InnoDB;
@@ -21,7 +20,7 @@ CREATE TABLE IF NOT EXISTS `ece356`.`user` (
 -- Table `ece356`.`doctor`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `ece356`.`doctor` (
-    `user_id` INT NOT NULL AUTO_INCREMENT,
+    `user_id` VARCHAR(45) NOT NULL,
     `pay_rate` FLOAT NOT NULL,
     PRIMARY KEY (`user_id`),
     FOREIGN KEY (`user_id`)
@@ -34,14 +33,14 @@ CREATE TABLE IF NOT EXISTS `ece356`.`doctor` (
 -- Table `ece356`.`patient`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `ece356`.`patient` (
-    `user_id` INT NOT NULL,
+    `user_id` VARCHAR(45) NOT NULL,
     `version_number` INT NOT NULL,
     `phone_number` VARCHAR(15) NOT NULL,
     `health_card` CHAR(12) NOT NULL,
     `sin` CHAR(9) NOT NULL,
     `address` VARCHAR(45) NOT NULL,
     `current_health` SMALLINT NOT NULL,
-    `doctor_account` INT NOT NULL,
+    `doctor_account` VARCHAR(45) NOT NULL,
     PRIMARY KEY (`version_number` , `user_id`),
     INDEX `doctor_account_idx` (`doctor_account` ASC),
     FOREIGN KEY (`user_id`)
@@ -57,8 +56,8 @@ CREATE TABLE IF NOT EXISTS `ece356`.`patient` (
 -- Table `ece356`.`pati_doct`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `ece356`.`pati_doct` (
-    `doctor_account` INT NOT NULL,
-    `patient_account` INT NOT NULL,
+    `doctor_account` VARCHAR(45) NOT NULL,
+    `patient_account` VARCHAR(45) NOT NULL,
     `patient_version_number` INT NOT NULL,
     PRIMARY KEY (`doctor_account` , `patient_account` , `patient_version_number`),
     INDEX `version_number_idx` (`patient_version_number` ASC),
@@ -79,8 +78,8 @@ CREATE TABLE IF NOT EXISTS `ece356`.`pati_doct` (
 -- Table `ece356`.`user_doct`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `ece356`.`user_doct` (
-    `user_id` INT NOT NULL AUTO_INCREMENT,
-    `doctor_account` INT NOT NULL,
+    `user_id` VARCHAR(45) NOT NULL,
+    `doctor_account` VARCHAR(45) NOT NULL,
     PRIMARY KEY (`user_id` , `doctor_account`),
     FOREIGN KEY (`doctor_account`)
         REFERENCES `ece356`.`doctor` (`user_id`)
@@ -104,7 +103,7 @@ CREATE TABLE IF NOT EXISTS `ece356`.`legal_prescription` (
 -- Table `ece356`.`doct_lega`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `ece356`.`doct_lega` (
-    `doctor_user` INT NOT NULL AUTO_INCREMENT,
+    `doctor_user` VARCHAR(45) NOT NULL,
     `legal_prescription_medication` INT NOT NULL,
     PRIMARY KEY (`doctor_user` , `legal_prescription_medication`),
     INDEX `medication_idx` (`legal_prescription_medication` ASC),
@@ -121,9 +120,9 @@ CREATE TABLE IF NOT EXISTS `ece356`.`doct_lega` (
 -- Table `ece356`.`appointment`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `ece356`.`appointment` (
-    `patient_account` INT NOT NULL,
+    `patient_account` VARCHAR(45) NOT NULL,
     `patient_version_number` INT NOT NULL,
-    `doctor_account` INT NOT NULL,
+    `doctor_account` VARCHAR(45) NOT NULL,
     `version_number` INT NOT NULL AUTO_INCREMENT,
     `start_time` DATETIME NOT NULL,
     `end_time` DATETIME NOT NULL,
@@ -144,7 +143,7 @@ CREATE TABLE IF NOT EXISTS `ece356`.`appointment` (
 -- Table `ece356`.`visit`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `ece356`.`visit` (
-    `appointment_patient_account` INT NOT NULL,
+    `appointment_patient_account` VARCHAR(45) NOT NULL,
     `appointment_patient_version_number` INT NOT NULL,
     `appointment_version_number` INT NOT NULL,
     `appointment_start_time` DATETIME NOT NULL,
@@ -162,7 +161,7 @@ CREATE TABLE IF NOT EXISTS `ece356`.`visit` (
 -- Table `ece356`.`lega_visi`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `ece356`.`lega_visi` (
-    `visit_appointment_patient_account` INT NOT NULL,
+    `visit_appointment_patient_account` VARCHAR(45) NOT NULL,
     `visit_appointment_patient_version_number` INT NOT NULL,
     `visit_appointment_version_number` INT NOT NULL,
     `visit_appointment_start_time` DATETIME NOT NULL,
@@ -187,7 +186,7 @@ CREATE TABLE IF NOT EXISTS `ece356`.`lega_visi` (
 -- Table `ece356`.`treatment`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `ece356`.`treatment` (
-    `visit_appointment_patient_account` INT NOT NULL AUTO_INCREMENT,
+    `visit_appointment_patient_account` VARCHAR(45) NOT NULL,
     `visit_appointment_patient_version_number` INT NOT NULL,
     `visit_appointment_version_number` INT NOT NULL,
     `visit_appointment_start_time` TIMESTAMP NOT NULL,
--- project/src/main/resources/db/populateDB.sql
+++ project/src/main/resources/db/populateDB.sql
@@ -6,14 +6,14 @@
 -- INSERT IGNORE INTO vets VALUES (6, 'Sharon', 'Jenkins');
 
 
-INSERT IGNORE INTO user VALUES (1, 'password', 'daskinne', 'Dr. Dave');
-INSERT IGNORE INTO user VALUES (2, 'password', 'pat', 'Patient Pat');
-INSERT IGNORE INTO user VALUES (3, 'password', 'steve', 'Staffer Steve');
-INSERT IGNORE INTO user VALUES (4, 'password', 'admin', 'Adam the Admin');
-INSERT IGNORE INTO user VALUES (5, 'password', 'dan', 'Dr. Dan');
+INSERT IGNORE INTO user VALUES ('daskinne', 'password', 'Dr. Dave');
+INSERT IGNORE INTO user VALUES ('pat', 'password',  'Patient Pat');
+INSERT IGNORE INTO user VALUES ('steve', 'password', 'Staffer Steve');
+INSERT IGNORE INTO user VALUES ('admin', 'password', 'Adam the Admin');
+INSERT IGNORE INTO user VALUES ('dan', 'password','Dr. Dan');
 
-INSERT IGNORE INTO doctor VALUES (1, 9999);
-INSERT IGNORE INTO doctor VALUES (5, 10000);
+INSERT IGNORE INTO doctor VALUES ('daskinne', 9999);
+INSERT IGNORE INTO doctor VALUES ('dan', 10000);
 
 -- Health status => ENUM
 -- 0 => Dead
@@ -22,7 +22,7 @@ INSERT IGNORE INTO doctor VALUES (5, 10000);
 INSERT INTO patient (user_id, version_number, phone_number, health_card, sin, address, current_health, doctor_account) values (2,0,'4152002000','123456789012','123456789','Hospital Ave.',0,1);
 
 -- Assign patient pat to Doctor Dan
-INSERT IGNORE INTO pati_doct VALUES (5,2,0);
+INSERT IGNORE INTO pati_doct VALUES ('dan','pat',0);
 
 
 -- Create 3 Prescriptions
@@ -33,8 +33,8 @@ INSERT IGNORE INTO legal_prescription VALUES (2);
 INSERT IGNORE INTO legal_prescription VALUES (3);
 
 -- Assign a legal perscription to Dr. Dave
-INSERT IGNORE INTO doct_lega VALUES (1,1);
+INSERT IGNORE INTO doct_lega VALUES ('daskinne',1);
 
 -- Assign Staffer Steve to Dr. Dave
-INSERT IGNORE INTO user_doct VALUES (3,1);
+INSERT IGNORE INTO user_doct VALUES ('steve','daskinne');
 
