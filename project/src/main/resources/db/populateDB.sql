a7eee7345420ed53398d62d5b2834beba76a0d7c
David Skinner <david.skinnera@gmail.com>
2014-03-23 21:54:27 -0400

    fix the build

--- project/src/main/resources/db/populateDB.sql
+++ project/src/main/resources/db/populateDB.sql
@@ -20,7 +20,6 @@ INSERT IGNORE INTO doctor VALUES ('dan', 10000);
 -- 0 => Dead
 -- 1 => Sick
 -- 3 => Healthy
-TRUNCATE patient;
 INSERT INTO patient values ('pat',0,'4152002000','123456789012','123456789','Hospital Ave.',0,'daskinne');
 INSERT INTO patient values ('pat',1,'4152002001','000000000001','123456789','Hospital Ave.',0,'daskinne');
 INSERT INTO patient values ('pat',2,'4152002002','000000000002','123456789','Hospital Ave.',0,'daskinne');
